40e0bc85ac986fccb66f235d9176a757de1c0bc0
Jonatasvangroll <jonatas.vangroll@gmail.com>
2022-03-16 19:59:25 -0300

    Update Atividade.java
    alterado

--- Atividade.java
+++ Atividade.java
@@ -3,29 +3,87 @@ import java.util.List;
 
 public class Atividade{
 
-    public static void main (String []args){
-       int[] lista = {1,3,5,7};
-       int[] resultado = mergeSort(lista);
-       System.out.println(resultado.length);
+  public static void main(String args[]) {
+    int arr[] = { 6, 5, 12, 10, 9, 1 };
 
-       for(int i=0;i<resultado.length;i++){
-           System.out.println(resultado[i]);
+    MergeSort ob = new MergeSort();
+    ob.mergeSort(arr, 0, arr.length - 1);
 
-       }
-    }
-
-    public static int [] mergeSort(int [] lista ) {
-        if(lista.length==1){
-            return lista;
-        }
-        
-        
-        int A [] = Arrays.copyOfRange(lista, 0, lista.length/2);
-
-        int B [] = Arrays.copyOfRange(lista, (lista.length/2), lista.length);
-
-        return B;
-
-    }
-
+    System.out.println("Sorted array:");
+    printArray(arr);
+  }
 }
+
+class MergeSort {
+
+  
+  void merge(int arr[], int p, int q, int r) {
+
+    
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    int L[] = new int[n1];
+    int M[] = new int[n2];
+
+    for (int i = 0; i < n1; i++)
+      L[i] = arr[p + i];
+    for (int j = 0; j < n2; j++)
+      M[j] = arr[q + 1 + j];
+
+    
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = p;
+
+    
+    while (i < n1 && j < n2) {
+      if (L[i] <= M[j]) {
+        arr[k] = L[i];
+        i++;
+      } else {
+        arr[k] = M[j];
+        j++;
+      }
+      k++;
+    }
+
+    
+    while (i < n1) {
+      arr[k] = L[i];
+      i++;
+      k++;
+    }
+
+    while (j < n2) {
+      arr[k] = M[j];
+      j++;
+      k++;
+    }
+  }
+
+  
+  void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+
+     
+      int m = (l + r) / 2;
+
+      mergeSort(arr, l, m);
+      mergeSort(arr, m + 1, r);
+
+      
+      merge(arr, l, m, r);
+    }
+  }
+
+
+  void printArray(int arr[]) {
+    int n = arr.length;
+    for (int i = 0; i < n; ++i)
+      System.out.print(arr[i] + " ");
+    System.out.println();
+  }
+
+ }
